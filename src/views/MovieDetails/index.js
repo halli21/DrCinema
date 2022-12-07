01e4a32f95cc83b80e9675fa1238214983e91d1e
arnarbragi <arnarbb21@ru.is>
2022-12-07 10:01:59 +0000

    showtime list title

--- src/views/MovieDetails/index.js
+++ src/views/MovieDetails/index.js
@@ -62,6 +62,8 @@ const MovieDetails = ( {route,  navigation: {navigate} } ) => {
         <Text style={styles.duration}>{movie.durationMinutes} min.</Text>
         <View style={styles.line}><Text style={styles.plot}>{movie.plot}</Text></View>
         <Text style={styles.genres}>{genreString}</Text>
+
+        <Text style={styles.showTitle}>Showtimes:</Text>
         <ShowtimeList showtimes={showtimes} />
     
     </ScrollView>
